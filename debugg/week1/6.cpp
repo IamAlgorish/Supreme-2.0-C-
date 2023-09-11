21fb25d95a2cb0af13ff6ac65d7952d883d87dfa
Rishabh jha <92349930+Rishabhjha1421@users.noreply.github.com>
2023-09-12 04:01:27 +0530

    solved problem for week 1
    its debugg questions sollution

---
+++ debugg/week1/6.cpp
@@ -0,0 +1,25 @@
+/*
+Pattern
+N = 5
+E
+DE
+CDE
+BCDE
+ABCDE
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i,j,n;;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        
+        char p='A'+n-i;
+        for(j=1;j<=i;j++){
+            cout<<p;
+            p++;  
+        }
+        cout<<endl;
+    }
+}
