e56c8803230d0ed3caa2800dacc5f4d4da5a1ac8
Rishabh jha <92349930+Rishabhjha1421@users.noreply.github.com>
2023-09-19 00:57:21 +0530

    Update getfirst.cpp

--- misclanious/getfirst.cpp
+++ misclanious/getfirst.cpp
@@ -1,3 +1,16 @@
+/*Given an integer N. The task is to return the position of first set bit found from the right side in the binary representation of the number.
+Note: If there is no set bit in the integer N, then return 0 from the function.  
+
+Example 1:
+
+Input: 
+N = 18
+Output: 
+2
+Explanation: 
+Binary representation of 18 is 010010,the first set bit from the right side is at position 2.*/
+
+
 class Solution
 {
     public:
@@ -5,6 +18,7 @@ class Solution
     unsigned int getFirstSetBit(int n)
     {
 
-    return log2(n & -n) + 1;
+    return log2(n & -n) + 1; //solve it in 0(1) time complexities
+        
     }
 };
