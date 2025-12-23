c35a5d91cee4dcb6d2b81e428770b6270d1bbd59
AlgoRish <92349930+IamAlgorish@users.noreply.github.com>
2025-12-23 15:27:50 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (18.19%) - LeetSync

--- Leetcode/Easy/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ Leetcode/Easy/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -2,28 +2,16 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int track = 0;
-       int counter = 0;
-        int j = 1;
-           for(int i = 0 ; i < nums.size()-1;i++)
-        {
-            if(nums[i]==nums[i+1])
-            {
-                counter++;
-            }}
         for(int i = 0 ; i < nums.size();i++)
         {
            
-            if(nums[track]==nums[i])
+            if(nums[track]!=nums[i])
             {
-                
-            }
-            else
-            {
-                track++;
+                 track++;
                 nums[track] = nums[i];
             }
         }
         
-        return nums.size()-counter;
+        return track+1;
            }
 };
