3f5b4cdb605feb69df4374efe9708aaaea63ae85
AlgoRish <92349930+IamAlgorish@users.noreply.github.com>
2026-05-02 22:20:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (66.14%) - LeetSync

---
+++ Leetcode/Easy/804-rotated-digits/rotated-digits.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isGood(int num) {
+        bool changed = false;
+
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit == 3 || digit == 4 || digit == 7)
+                return false;
+            if (digit == 2 || digit == 5 || digit == 6 || digit == 9)
+                changed = true;
+            num /= 10;
+        }
+
+        return changed;
+    }
+
+    int rotatedDigits(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (isGood(i))
+                count++;
+        }
+
+        return count;
+    }
+};
